0ceb607835ddd032660e41f4ec18004b98d692b9
DixonOrtiz <dixon.ortizc@gmail.com>
2020-02-12 22:55:30 -0300

    Carousel finished

--- src/assets/jss/material-kit-react/views/componentsSections/carouselStyle.js
+++ src/assets/jss/material-kit-react/views/componentsSections/carouselStyle.js
@@ -1,6 +1,6 @@
 import { container } from "assets/jss/material-kit-react.js";
 
-const carouselStyle = {
+const carouselStyle = theme => ({
   section: {
     padding: "70px 0"
   },
@@ -8,7 +8,22 @@ const carouselStyle = {
   marginAuto: {
     marginLeft: "auto !important",
     marginRight: "auto !important"
+  },
+
+  slickImage: {
+    width: "100% !important",
+    display: "inline-flex !important",
+
+    [theme.breakpoints.up("xs")]: {
+      height: "200px"
+    },
+    [theme.breakpoints.up("sm")]: {
+      height: "350px"
+    },
+    [theme.breakpoints.up("md")]: {
+      height: "500px"
+    }
   }
-};
+});
 
 export default carouselStyle;
--- src/views/Components/Sections/SectionCarousel.js
+++ src/views/Components/Sections/SectionCarousel.js
@@ -8,9 +8,8 @@ import LocationOn from "@material-ui/icons/LocationOn";
 // core components
 import GridContainer from "components/Grid/GridContainer.js";
 import GridItem from "components/Grid/GridItem.js";
-import Card from "components/Card/Card.js";
 
-import image1 from "assets/img/bg.jpg";
+import image1 from "assets/img/ioled-banner.png";
 import image2 from "assets/img/bg2.jpg";
 import image3 from "assets/img/bg3.jpg";
 
@@ -33,7 +32,11 @@ export default function SectionCarousel() {
       <GridItem xs={12} sm={12} md={12} className={classes.marginAuto}>
         <Carousel {...settings}>
           <div>
-            <img src={image1} alt="First slide" className="slick-image" />
+            <img
+              src={image1}
+              alt="First slide"
+              className={classes.slickImage}
+            />
             <div className="slick-caption">
               <h4>
                 <LocationOn className="slick-icons" />
@@ -42,7 +45,11 @@ export default function SectionCarousel() {
             </div>
           </div>
           <div>
-            <img src={image2} alt="Second slide" className="slick-image" />
+            <img
+              src={image2}
+              alt="Second slide"
+              className={classes.slickImage}
+            />
             <div className="slick-caption">
               <h4>
                 <LocationOn className="slick-icons" />
@@ -51,7 +58,11 @@ export default function SectionCarousel() {
             </div>
           </div>
           <div>
-            <img src={image3} alt="Third slide" className="slick-image" />
+            <img
+              src={image3}
+              alt="Third slide"
+              className={classes.slickImage}
+            />
             <div className="slick-caption">
               <h4>
                 <LocationOn className="slick-icons" />
