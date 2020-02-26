a71d7344026487a6e0d2666636b7b8aa57808086
DixonOrtiz <dixon.ortizc@gmail.com>
2020-02-26 11:14:54 -0300

    details in app.js fixed

--- src/views/App.js
+++ src/views/App.js
@@ -16,7 +16,6 @@ export default function App() {
       <SectionHeader />
       <SectionWhatsIoled />
       <SectionVideo />
-
       <SectionInformationParameters />
       <SectionFooter />
     </React.Fragment>
